0ea5e2679475b00ba3a21e90c12365fa727a9940
viceRockea <vicentecancinoprek@gmail.com>
2026-01-26 23:53:36 -0300

    se modifico el main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 1. Habilitar CORS
-  app.enableCors({
-    origin: [
-      'http://localhost:5173', // Tu PC local
-      'https://inventario-frenos-frontend.vercel.app', // Tu Vercel (¡Sin barra al final!)
-      process.env.FRONTEND_URL, // La variable del .env (si la usas)
-    ].filter(Boolean), // Esto elimina valores "undefined" para evitar errores
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  // 2. Prefijo Global
+  // ESTA ES LA LÍNEA MÁGICA QUE NECESITAS
   app.setGlobalPrefix('api');
 
-  // 3. Validaciones
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  // Habilitar CORS para que Vercel pueda entrar
+  app.enableCors({
+    origin: '*', // O la URL de tu frontend en Vercel
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
 
-  // 4. Swagger
-  const config = new DocumentBuilder()
-    .setTitle('API Taller Frenos Aguilera')
-    .setDescription('Documentación del Backend')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
-  // 5. Iniciar Servidor (MODO DOCKER)
-  const port = process.env.PORT || 3000;
-  
-  // IMPORTANTE: '0.0.0.0' permite conexiones desde fuera del contenedor
-  await app.listen(port, '0.0.0.0');
-
-  console.log(`🚀 Servidor corriendo en puerto: ${port}`);
+  await app.listen(3000);
 }
-void bootstrap();
+bootstrap();
